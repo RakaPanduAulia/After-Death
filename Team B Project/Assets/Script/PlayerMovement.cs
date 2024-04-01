90e958a6f24c9b876c5c57993c50b0488f40d1de
RakaPanduAulia <rakapandu12@gmail.com>
2024-04-01 09:12:31 +0700

    Jump not fix

--- Team B Project/Assets/Script/PlayerMovement.cs
+++ Team B Project/Assets/Script/PlayerMovement.cs
@@ -1,15 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public LayerMask groundLayer; // Lapisan tanah
     public float moveSpeed = 5f; // Kecepatan gerakan pemain
+    public float jumpForce = 5f; // Kekuatan lompatan
+    private bool hasJumped = false; // Apakah pemain sudah melompat
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        Jump();
     }
 
     void MovePlayer()
@@ -18,9 +20,42 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
 
         // Membuat vektor gerakan dengan kecepatan
-        Vector3 movement = new Vector3(moveX, 0f, 0f) * moveSpeed * Time.deltaTime;
+        Vector2 movement = new Vector2(moveX, 0f) * moveSpeed * Time.deltaTime;
 
         // Menggerakkan pemain
-        transform.position += movement;
+        transform.Translate(movement);
     }
+
+    void Jump()
+    {
+        // Mengecek apakah pemain menekan tombol lompat dan apakah berada di tanah
+        if (Input.GetButtonDown("Jump") && !hasJumped && IsGrounded())
+        {
+            // Menambahkan kekuatan lompat ke arah vertikal
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            hasJumped = true; // Pemain sudah melompat
+        }
+    }
+
+    // Mengecek apakah pemain berada di tanah
+    bool IsGrounded()
+    {
+        // Menentukan area kecil di bawah pemain untuk mendeteksi tanah
+        Vector2 position = transform.position;
+        float distance = 0.1f;
+        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, distance, groundLayer);
+
+        return hit.collider != null; // Kembalikan true jika ada tabrakan dengan lapisan tanah
+    }
+
+    // Dijalankan ketika pemain menyentuh tanah
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (IsGrounded())
+        {
+            hasJumped = false; // Reset status lompatan jika pemain menyentuh tanah
+        }
+    }
+
+    
 }
