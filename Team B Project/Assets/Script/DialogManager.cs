0ddeeb7760e5e800a60374316fd81fe6d0c8f65a
RakaPanduAulia <rakapandu12@gmail.com>
2024-04-16 16:08:44 +0700

    UpdatedDialogSystem
    Aded a timer for the player, and other minor changes

--- Team B Project/Assets/Script/DialogManager.cs
+++ Team B Project/Assets/Script/DialogManager.cs
@@ -8,14 +8,22 @@ public class DialogManager : MonoBehaviour
 {
     public Text displayText;
     public InputField answerInput;
+    public Slider timeSlider;
+    public GameObject arrowIndicator;
     private string correctAnswer = "Jakarta";
     private Queue<string> dialogLines = new Queue<string>();
+    private float timeLeft = 10f;
+    private bool isWaitingForInput = false;
+    private float delayBeforeNextLine = 0.25f;
+    private float timer = 0f;
 
     void Start()
     {
         SetupDialog();
         StartDialog();
         answerInput.gameObject.SetActive(false);
+        timeSlider.gameObject.SetActive(false);
+        arrowIndicator.SetActive(false);
         answerInput.onValueChanged.AddListener(delegate { ValidateText(answerInput.text); });
         answerInput.onEndEdit.AddListener(delegate { AttemptSubmit(answerInput); });
     }
@@ -28,13 +36,34 @@ public class DialogManager : MonoBehaviour
         dialogLines.Enqueue("Apa ibukota dari Indonesia?");
     }
 
+    void Update()
+    {
+        if (isWaitingForInput && timer <= 0)
+        {
+            arrowIndicator.SetActive(true); // Tampilkan panah saat pemain dapat menekan Space
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                arrowIndicator.SetActive(false); // Nonaktifkan panah segera setelah Space ditekan
+                ContinueDialog();
+            }
+        }
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            arrowIndicator.SetActive(false); // Sembunyikan panah selama hitungan mundur timer
+        }
+    }
+
     void StartDialog()
     {
-        Debug.Log("StartDialog called. Remaining dialog lines: " + dialogLines.Count);
-        string line = dialogLines.Dequeue();
-        Debug.Log("Processing line: " + line);
-        // StopAllCoroutines();
-        StartCoroutine(TypeSentence(line));
+        if (dialogLines.Count > 0)
+        {
+            Debug.Log("StartDialog called. Remaining dialog lines: " + dialogLines.Count);
+            string line = dialogLines.Dequeue();
+            Debug.Log("Processing line: " + line);
+            StartCoroutine(TypeSentence(line));
+        }
+        
     }
 
     IEnumerator TypeSentence(string sentence)
@@ -45,19 +74,19 @@ public class DialogManager : MonoBehaviour
             displayText.text += letter;
             yield return new WaitForSeconds(0.05f);
         }
-        
-        // Jika dialog terakhir adalah pertanyaan, tunggu jawaban.
+
+        // If this is the last line, automatically continue after delay
         if (dialogLines.Count == 0)
         {
-            Debug.Log("Finished all dialogs, enabling input field...");
-            correctAnswer = "Jakarta"; // Setelah semua dialog selesai.
+            yield return new WaitForSeconds(delayBeforeNextLine);
             EnableInput();
+            StartTimer();
         }
         else
         {
-            Debug.Log("There are more dialogs to process.");
-            yield return new WaitForSeconds(1); // Tunggu sebentar sebelum dialog berikutnya.
-            StartDialog();
+            yield return new WaitForSeconds(delayBeforeNextLine);
+            isWaitingForInput = true;
+            timer = delayBeforeNextLine;
         }
     }
 
@@ -73,14 +102,15 @@ public class DialogManager : MonoBehaviour
         if (answerInput.text.Trim().ToLower() == correctAnswer.ToLower())
         {
             Debug.Log("Jawaban Benar!");
-            answerInput.DeactivateInputField();
-            answerInput.onValueChanged.RemoveAllListeners();
-            answerInput.onEndEdit.RemoveAllListeners();
+            CompleteAnswering();
         }
         else
         {
             Debug.Log("Jawaban Salah, coba lagi.");
+            // Jangan deactivate input field; biarkan pemain terus mencoba
+            answerInput.text = ""; // Bersihkan field untuk jawaban baru
             answerInput.ActivateInputField();
+            answerInput.Select();
         }
     }
 
@@ -105,5 +135,54 @@ public class DialogManager : MonoBehaviour
             // Teks berwarna hitam jika benar
             answerInput.textComponent.color = Color.black;
         }
+    }
+
+    void StartTimer()
+    {
+        timeSlider.gameObject.SetActive(true);
+        timeSlider.maxValue = timeLeft;
+        timeSlider.value = timeLeft;
+        StartCoroutine(UpdateTimer());
+    }
+
+    IEnumerator UpdateTimer()
+    {
+        while (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timeSlider.value = timeLeft;
+            yield return null;
+        }
+        TimeOut();
+    }
+
+    void TimeOut() 
+    {
+        Debug.Log("Time is up");
+        CompleteAnswering();
+    }
+
+    void ContinueDialog()
+    {
+        if (dialogLines.Count > 0)
+        {
+            string line = dialogLines.Dequeue();
+            StartCoroutine(TypeSentence(line));
+        }
+        else
+        {
+            EnableInput();
+            StartTimer();
+        }
+        isWaitingForInput = false;
+    }
+
+    private void CompleteAnswering()
+    {
+        answerInput.DeactivateInputField();
+        answerInput.onValueChanged.RemoveAllListeners();
+        answerInput.onEndEdit.RemoveAllListeners();
+        StopAllCoroutines(); // Stop the timer
+        timeSlider.gameObject.SetActive(false); // Hide the timer slider
     }
 }
---
+++ Team B Project/Assets/Script/ScreenShake.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScreenShake : MonoBehaviour
+{
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
